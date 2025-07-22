52a220b7950ada85674e1c2a8368854bd8a09ffb
Rahat Adnan <sthep90@gmail.com>
2025-07-22 23:19:31 +0600

    creating Render pass

--- src/vulkankit/RenderV.cpp
+++ src/vulkankit/RenderV.cpp
@@ -594,9 +594,12 @@ void RenderV::createRenderPass() {
   VkAttachmentDescription colorAttachment = {};
   colorAttachment.format = this->swapChainImageFormat;  //?format to use in attachment
   colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;  //? Count of sample to learn for multisampling
-  colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-  colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-  colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+  colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR; //? what to do with attachment before rendering
+  colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE; //? what to do with attachment after rendering
+  colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE; //? what to do with stencil before rendering
+  colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;//? what to do with stencil after rendering
+  colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED; //? image data layout before render pass start
+  colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;//? image data will change to it after render pass
 
   VkRenderPassCreateInfo renderpassCreateInfo = {};
   renderpassCreateInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
