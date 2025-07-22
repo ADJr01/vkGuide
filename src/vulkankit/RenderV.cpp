dd076cde15d09e178af55a65320315f30658aa27
Rahat Adnan <sthep90@gmail.com>
2025-07-22 22:16:11 +0600

    creating Render pass

--- src/vulkankit/RenderV.cpp
+++ src/vulkankit/RenderV.cpp
@@ -594,7 +594,9 @@ void RenderV::createRenderPass() {
   VkAttachmentDescription colorAttachment = {};
   colorAttachment.format = this->swapChainImageFormat;  //?format to use in attachment
   colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;  //? Count of sample to learn for multisampling
-  colorAttachment.loadOp =
+  colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+  colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+  colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
 
   VkRenderPassCreateInfo renderpassCreateInfo = {};
   renderpassCreateInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
