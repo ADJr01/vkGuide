959a9a029deb820e8ac24406c5f577c933fbb9cc
Rahat Adnan <sthep90@gmail.com>
2025-07-29 01:00:53 +0600

    fence applied

--- src/vulkankit/RenderV.cpp
+++ src/vulkankit/RenderV.cpp
@@ -794,6 +794,7 @@ void RenderV::draw() {
   //#1: GEt Next Image to be drawn and get signal semaphore when ready to be drawn
   uint32_t imageIndex;
   vkAcquireNextImageKHR(this->Context.Device.logicalDevice,this->swapChain,std::numeric_limits<uint64_t>::max(),this->imageAvailableSemaphore[this->currentFrame],VK_NULL_HANDLE,&imageIndex);
+  vkWaitForFences(this->Context.Device.logicalDevice,1,&this->drawFences[this->currentFrame],VK_TRUE,std::numeric_limits<uint64_t>::max());
   //#2: Submit Command buffer to queue
   VkSubmitInfo submitInfo = {};
   submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
